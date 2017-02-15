4f69236633df2aae12c619d42f9236b4d0a19f68
Aidan Curtis <aidancurtis@MacBook-Pro-8.local>
2017-02-15 14:44:19 -0600

    updated database and hall effect post request

--- models/BikeData.js
+++ models/BikeData.js
@@ -3,15 +3,6 @@
 module.exports = function(sql, Sql) {
 var bikedata = sql.define("BikeData", {
         stamp: {type: Sql.BIGINT, primaryKey: true},
-        x: {type: Sql.FLOAT},
-        y: {type: Sql.FLOAT},
-        z: {type: Sql.FLOAT},
-        xG: {type: Sql.FLOAT},
-        yG: {type: Sql.FLOAT},
-        zG: {type: Sql.FLOAT},
-        xM: {type: Sql.FLOAT},
-        yM: {type: Sql.FLOAT},
-        zM: {type: Sql.FLOAT},
         rpm: {type: Sql.FLOAT},
         bikeId: {type: Sql.INTEGER}
 }, {timestamps: true,freezeTableName: true});
--- routers/bbb_router.js
+++ routers/bbb_router.js
@@ -17,6 +17,14 @@ router.get("/data", function(req, res){
         res.send(list);
     })
 });
+router.get("/data/last", function(req,res){
+	data.findOne({
+		order: "stamp DESC"
+	}).then(function(list){
+		res.setHeader('Content-Type', 'application/json');
+        res.send(list);
+	})
+})
 router.get("/data/:t1/:t2", function(req, res){
 	data.findAll({
 		where:
@@ -37,56 +45,11 @@ router.post("/addsession", function(req, res){
 	res.send("session created")
 });
 router.post("/bike", function(req, res){
-
-	var py = spawn('python', ['python/compute_rpm.py'])
-var dataString = ''
-var json = require('json')
-	/*Here we are saying that every time our node application receives data from the python process output stream(on 'data'), we want to convert that received data into a string and append it to the overall dataString.*/
-	py.stdout.on('data', function(data){
-  		dataString += data.toString();
-	});
-
-	/*Once the stream is done (on 'end') we want to simply log the received data to the console.*/
-	py.stdout.on('end', function(){
-	json = req.body
-	if(req.body){
-		console.log("dataString")
-		console.log(dataString)
-		rpm = parseFloat(dataString)
-		dataString = ''
-		rpm_array = []
-		timestamps = Object.keys(req.body)
-		console.log(timestamps)
-		for (idx in timestamps){
-			data.create({
-				stamp: parseFloat(timestamps[idx]),
-				x: json[timestamps[idx]][0],
-        		y: json[timestamps[idx]][1],
-        		z: json[timestamps[idx]][2],
-        		xG: json[timestamps[idx]][3],
-        		yG: json[timestamps[idx]][4],
-        		zG: json[timestamps[idx]][5],
-        		xM: json[timestamps[idx]][6],
-        		yM: json[timestamps[idx]][7],
-        		zM: json[timestamps[idx]][8],
-        		bikeId: json[timestamps[idx]][9],
-				rpm: rpm
-			})
-		}
-	}
-	res.send("Success")
-	});
-	py.on('exit', function(code) {
-    	console.log("Exited with code " + code);
-	});
-	py.stderr.on('data', function(data) {
-    	console.error(data.toString());
+	data.create({
+		stamp:  'NOW()',
+		rpm: req.body.rpm,
+		bikeId: req.body.bikeId
+	})
+	res.send("Upload Success")
 });
-	console.log("JSON_STRING")
-	console.log(JSON.stringify(req.body))
-	py.stdin.write(JSON.stringify(req.body));
-	 py.stdin.end();
-
-
-})
 module.exports = router; 
