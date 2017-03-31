d374bac105b2392d19f489bcb2ee464987300fce
Aidan Curtis <aidancurtis@MacBook-Pro-8.local>
2017-03-30 22:33:23 -0500

    demo1

--- routers/bbb_router.js
+++ routers/bbb_router.js
@@ -61,35 +61,92 @@ router.post("/logout", function(req, res){
         res.send({status: "success"});
 	})
 })
-router.post("/addsession", function(req, res){
-	user.findAll({
-		where: [{rfid : req.body.tag}]
-	}).then(function(list){
-		if(list.length == 0){
-			user.create(
-				{rfid: req.body.tag}
-				).then(function(user){
-					console.log(user)
-					session.create({
-						stampStart: new Date().getTime(),
-						userId: user.dataValues.id
-					})
-					res.send({status: "new"})
-
-				})
-			
-		}
-		else{
-			res.send({status: "old", user: list[0]})
-			session.create({
-				stampStart: new Date().getTime(),
-				userId: list[0].dataValues.id
-			})
-		}
-		
-		
-	})
+router.post("/addsession", function(req, res) {
+    session.findAll(
+        {where: {
+            stampEnd: null
+        }}).then(function(list) {
+        	if(list.length == 0){
+        user.findAll({
+            where: [{
+                rfid: req.body.tag
+            }]
+        }).then(function(list) {
+            if (list.length == 0) {
+                user.create({
+                    rfid: req.body.tag
+                }).then(function(user) {
+                    console.log(user)
+                    session.create({
+                        stampStart: new Date().getTime(),
+                        userId: user.dataValues.id
+                    })
+                    res.send({
+                        status: "new"
+                    })
+                })
+            } else {
+                res.send({
+                    status: "old",
+                    user: list[0]
+                })
+                session.create({
+                    stampStart: new Date().getTime(),
+                    userId: list[0].dataValues.id
+                })
+            }
+        })
+    }
+    else{
+    	res.send({status: "busy"})
+    }
+})
+   
+    
 });
+String.prototype.toHHMMSS = function () {
+    var sec_num = parseInt(this, 10); // don't forget the second param
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return hours+':'+minutes+':'+seconds;
+}
+router.get("/average_duration", function(req, res){
+	session.findAll().then(function(sessions){
+		var total_dur = 0
+		var count = 0
+		for(inc in sessions){
+			var start = sessions[inc].stampStart
+			var end = sessions[inc].stampEnd
+			if(start!= null && end!= null){
+			count = count+1
+
+			console.log(end-start)
+			if(end-start>=337645142){
+				console.log("pk")
+				console.log(end)
+				console.log(start)
+			}
+			total_dur = total_dur + parseInt(end-start)
+			}
+		}
+		res.send({success: true, duration: String(total_dur/parseFloat(count*1000)).toHHMMSS()})
+	})
+})
+router.get("/workout_duration", function(req, res){
+	session.findOne(
+        {where: {
+            stampEnd: null
+        }}).then(function(ses) {
+        	var start = ses.stampStart
+			var end = new Date().getTime()
+        	res.send({success: true, duration: String((end-start)/parseFloat(1000))})
+        });
+})
 router.post("/addname", function(req, res){
 
 	user.update({
