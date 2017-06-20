00667e501329bb2ce31ebbe5ac672984344d552b
Rushi Bhalani <rushi.bhalani@gmail.com>
2017-06-20 13:46:38 -0500

    Changed userID in SessionData to be the primary key.

--- models/SessionData.js
+++ models/SessionData.js
@@ -2,9 +2,9 @@
 
 module.exports = function(sql, Sql) {
 var SessionData = sql.define("SessionData", {
+	    userID: {type: Sql.INTEGER, primaryKey: true},
         stampStart: {type: Sql.STRING},
-        stampEnd: {type: Sql.STRING},
-        userID: {type: Sql.INTEGER}
+        stampEnd: {type: Sql.STRING}
     }, {timestamps: false, freezeTableName: true});
     return SessionData;
 };
