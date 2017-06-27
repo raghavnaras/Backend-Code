febd5ddfd356d0e4d481047edb14fb62e8caca8c
haleyko <haleyko@rice.edu>
2017-06-27 15:45:26 -0500

    Update SessionData.js

--- models/SessionData.js
+++ models/SessionData.js
@@ -2,11 +2,11 @@
 
 module.exports = function(sql, Sql) {
 var SessionData = sql.define("SessionData", {
-			RFID: {type: Sql.BIGINT, primaryKey: true},
-	    userID: {type: Sql.INTEGER},
-			machineID: {type: Sql.INTEGER},
-      stampStart: {type: Sql.STRING},
-      stampEnd: {type: Sql.STRING}
+	RFID: {type: Sql.BIGINT, primaryKey: true},
+	userID: {type: Sql.INTEGER},
+	machineID: {type: Sql.INTEGER},
+	stampStart: {type: Sql.STRING},
+	stampEnd: {type: Sql.STRING}
     }, {timestamps: false, freezeTableName: true});
     return SessionData;
 };
