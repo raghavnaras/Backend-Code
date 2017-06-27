d4ada4ec4e076d67f462cc1d7b14c9d334129b4a
haleyko <haleyko@rice.edu>
2017-06-27 16:14:23 -0500

    synchronize type of RFID in the DB table

--- models/BikeData.js
+++ models/BikeData.js
@@ -5,7 +5,7 @@ var BikeData = sql.define("BikeData", {
         stamp: {type: Sql.BIGINT, primaryKey: true},
         rpm: {type: Sql.FLOAT},
         bikeID: {type: Sql.INTEGER},
-        sessionID: {type: Sql.INTEGER}
+        sessionID: {type: Sql.BIGINT}
 }, {timestamps: false, freezeTableName: true});
     return BikeData;
 };
