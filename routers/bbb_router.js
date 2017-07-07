69076a067aefc3061267ef37595716b2adb8abfe
haleyko <haleyko@rice.edu>
2017-07-07 13:29:41 -0500

    test to display the last data points on the radial and the chart

--- routers/bbb_router.js
+++ routers/bbb_router.js
@@ -93,7 +93,7 @@ router.post("/data/last", function(req,res){
 				],
 				where: {sessionID: session.sessionID}
 			}).then(function(bikeData){
-				console.log("last bike stamp", bikeData.stamp);
+				console.log("last bike stamp is", bikeData.stamp);
 				res.send({success: true, rpm: bikeData.rpm})
 			})
 		}
