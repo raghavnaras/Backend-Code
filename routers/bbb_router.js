0c26b6483b6e787eaaa539f67b93d355ed793cc7
Rushi Bhalani <rushi.bhalani@gmail.com>
2017-07-11 17:19:26 -0500

    Debugging errors in 'bbb_router.js'

--- routers/bbb_router.js
+++ routers/bbb_router.js
@@ -428,6 +428,7 @@ router.post("/process_tag", function(req, res) {
 
 router.post("/check_tag", function(req, res) {
 	registerTag(req.body.tagName, req.body.userID, req.body.machineID).then(function(pair) {
+		console.log("CHECK TAG PAIR: " + pair);
 		if (pair[0] > 0) {
 			res.send({status: "success"})
 		} else {
