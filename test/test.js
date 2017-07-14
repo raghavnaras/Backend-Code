234aa3f0d46264c9e3ae5992d95a456095453e17
Rushi Bhalani <rushi.bhalani@gmail.com>
2017-07-14 17:08:31 -0500

    Started unit testing backend code.

--- test/test.js
+++ test/test.js
@@ -1,17 +1,30 @@
-var assert = require('chai').assert;
+var chai = require('chai');
+var chaiHTTP = require('chai-http');
+var API_ENDPOINT = "http://52.34.141.31:8000/bbb";
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
+chai.use(chaiHTTP);
+
+var assert = chai.assert;
+var expect = chai.expect;
+
+
+// GET REQUESTS
+
+// test_connection
+describe('Server Connections', function() {
+	describe('test_connection', function() {
+		it('should return status 200 and \"success\"', function(done) {
+			chai.request(API_ENDPOINT)
+				.get('/test_connection')
+				.end(function (err, res) {
+ 					expect(err).to.be.null;
+     				expect(res).to.have.status(200);
+     				assert.equal(res.body.status, "success");
+     				done();
+				})
+
+		});
+	});
 });
 
-describe('Server Connections' function() {
-	describe('test_connection', function() {
-		it('should return \"success\"', function() {
-			assert.equal()
-		})
-	})
-})
+// 
