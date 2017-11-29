c6b2c0adc31dddf0848ecf39f65dca45b8e7b7ca
Aidan Curtis <>
2017-11-28 22:19:58 -0600

    hhmmss

--- routers/bbb_router.js
+++ routers/bbb_router.js
@@ -123,9 +123,9 @@ String.prototype.toHHMMSS = function () {
     var minutes = Math.floor((sec_num - (hours * 3600)) / 60)
     var seconds = Math.floor(sec_num - (hours * 3600) - (minutes * 60))
 
-    if (hours   < 10) {hours   = "0" + hours;}
-    if (minutes < 10) {minutes = "0" + minutes;}
-    if (seconds < 10) {seconds = "0" + seconds;}
+    if (hours   < 10) {hours   = "0" + String(hours);}
+    if (minutes < 10) {minutes = "0" + String(minutes);}
+    if (seconds < 10) {seconds = "0" + String(seconds);}
     return (hours + ':' + minutes + ':' + seconds);
 }
 
